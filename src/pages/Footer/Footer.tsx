d4aa9d7c082ab91f3c769f64daeae0d0cb06213f
mtake986 <tosa9629@gmail.com>
2023-03-02 09:45:32 -0700

    Footer font size

--- src/pages/Footer/Footer.tsx
+++ src/pages/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="text-center w-full absolute bottom-0 p-1 text-gray-500 bg-gray-200 text-sm">
+    <div className="text-center w-full absolute bottom-0 p-1 text-gray-500 bg-gray-200 text-xs">
       Masahiro Takechi All Rights Reserved
     </div>
   );
